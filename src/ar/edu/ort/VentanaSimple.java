3376f0535297aac0ac9866b3bbb329dfe4b3f478
Pablo Andres (Acceleration) <Pablo.Andres@Acceleration.biz>
2020-06-21 16:52:01 -0300

    Some testing

--- src/ar/edu/ort/VentanaSimple.java
+++ src/ar/edu/ort/VentanaSimple.java
@@ -3,9 +3,9 @@ package ar.edu.ort;
 //Clase componente concreto
 public class VentanaSimple implements ComponenteVisual {
 
-	 public void dibujar() {
-	    	System.out.println("dibujo una ventana");
-	 }
+  public void dibujar() {
+      System.out.println("dibujo una ventana");
+  }
 	 
 	public String getDescripcion() {
 	    return "ventana simple";
